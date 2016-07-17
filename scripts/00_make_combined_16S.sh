23d9ebe81f286e239b24b4c8b0cba98a8af74b3e
Jason Stajich <jason@bioperl.org>
2016-07-17 15:17:56 -0700

    add some reminder about what this script does

--- scripts/00_make_combined_16S.sh
+++ scripts/00_make_combined_16S.sh
@@ -1,4 +1,7 @@
 #PBS -l nodes=1:ppn=1,mem=1gb -j oe
+
+# merge reads previously split by sample into super file, assume reads can be matched to sample by their ID, eg PREFIX_readnumber - our convention is PROJECT.SAMPLEID
+
 PWD=`pwd`
 PREFIX=`basename $PWD`"_16S"
 if [ ! -f $PREFIX.R1.fna ]; then
--- scripts/00_make_combined_ITS.sh
+++ scripts/00_make_combined_ITS.sh
@@ -1,4 +1,5 @@
 #PBS -l nodes=1:ppn=1,mem=1gb -j oe
+# merge reads previously split by sample into super file, assume reads can be matched to sample by their ID, eg PREFIX_readnumber - our convention is PROJECT.SAMPLEID
 PWD=`pwd`
 PREFIX=`basename $PWD`"_ITS"
 if [ ! -f $PREFIX.R1.fna ]; then
