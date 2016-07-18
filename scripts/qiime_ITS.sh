f6b52fcbd2033243ac56b4b1e721c91ff920abec
Jason Stajich <jason@bioperl.org>
2016-07-17 22:32:12 -0700

    taxa summary fixed

--- scripts/qiime_ITS.sh
+++ scripts/qiime_ITS.sh
@@ -21,6 +21,7 @@ OUT=qiime_ITS_openref
 TAXOUT=qiime_ITS_taxa_summary
 
 #Pick OTUs against open ref ITS, use mutitthreaded where possible
+if [ ! -d $OUT ]; then
 if [ $CPU ]; then
 pick_open_reference_otus.py -i $FWD -o $OUT -s 0.1 -p $PWD/its_params.txt -r $UNITEDB \
  --suppress_align_and_tree --otu_picking_method uclust --parallel --jobs_to_start $CPU
@@ -28,12 +29,13 @@ else
 pick_open_reference_otus.py -i $FWD -o $OUT -s 0.1 -p $PWD/its_params.txt -r $UNITEDB \
  --suppress_align_and_tree --otu_picking_method uclust
 fi
+fi
 
 #Summarize OTUs table
-biom summarize-table -i $OUT/otu_table_mc2_w_tax.biom
+biom summarize-table -i $OUT/otu_table_mc2_w_tax.biom > $PREFIX.FWD.biom_summarize.txt
 
 #Summarize Taxa from OTUs table
-summarize_taxa_through_plots.py -i $OUT/otu_table_mc2_w_tax.biom -o $TAXOUT
+summarize_taxa_through_plots.py -i $OUT/otu_table_mc2_w_tax.biom -o $TAXOUT -f
 
 # Plot Taxonomy Summary command 
-plot_$TAXOUT.py -i $TAXOUT/otu_table_mc2_w_tax_L2.txt,$TAXOUT/otu_table_mc2_w_tax_L3.txt,$TAXOUT/otu_table_mc2_w_tax_L4.txt,$TAXOUT/otu_table_mc2_w_tax_L5.txt,$TAXOUT/otu_table_mc2_w_tax_L6.txt -o $TAXOUT/taxa_summary_plots/
+plot_taxa_summary.py -i $TAXOUT/otu_table_mc2_w_tax_L2.txt,$TAXOUT/otu_table_mc2_w_tax_L3.txt,$TAXOUT/otu_table_mc2_w_tax_L4.txt,$TAXOUT/otu_table_mc2_w_tax_L5.txt,$TAXOUT/otu_table_mc2_w_tax_L6.txt -o $TAXOUT/taxa_summary_plots/
