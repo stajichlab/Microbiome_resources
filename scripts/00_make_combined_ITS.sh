d8c68c9fdc9baffaa2f5b9334a21e3c209e1e54a
Jason Stajich <jason@bioperl.org>
2016-11-10 15:53:07 -0800

    go with adaptor trimming

--- scripts/00_make_combined_ITS.sh
+++ scripts/00_make_combined_ITS.sh
@@ -1,6 +1,8 @@
 #PBS -l nodes=1:ppn=16,mem=8gb -j oe
 module load fastx_toolkit
-module load vsearch
+module load vsearch\
+# cutadapt is part of installed python
+
 # merge reads previously split by sample into super file, assume reads can be matched to sample by their ID, eg PREFIX_readnumber - our convention is PROJECT.SAMPLEID
 CPU=$PBS_NP
 if [ ! $CPU ]; then
@@ -15,13 +17,18 @@ REVNOCHIMERA=$PREFIX.R2.nochimeras.fna
 UCHIMECHIMERA=/srv/projects/db/UNITE/v7.1/uchime_reference_dataset/ITS1_ITS2_datasets/uchime_sh_refs_dynamic_develop_985_01.01.2016.ITS1.fasta
 
 # per https://docs.google.com/document/d/1b9sUBOhOzlJxvt6Uim3hQXwOghIhEKulREzoSv2nXlE/edit
-# trim first 26 bp of read
+# trim first 30 bp of read
 # whenusing Kabir's primers
 if [ ! -f $FWDNOCHIMERA ]; then
  if [ -f $REV.bz2 ]; then
    pbzip -d $REV.bz2
  elif [ ! -f $FWD ]; then
+  if [ -f rev_adaptors.fa && -f fwd_adaptors.fa ]; then
+    zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -a file:rev_adaptors.fa -g file:fwd_adaptors.fa -o $FWD -r $PREFIX.F1.rest.fna - > cutadapt_R1.log
+  else
   zcat ITS_R1/ITS.*.fa.gz | fasta_formatter | fastx_trimmer -f 30 | fastx_trimmer -t 29 > $FWD
+#    zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -o $FWD -r $PREFIX.F1.rest.fna - > cutadapt_R1.log
+  fi
  fi
  vsearch -uchime_ref $FWD -db $UCHIMECHIMERA -strand plus -nonchimeras $FWDNOCHIMERA -threads $CPU
  pbzip2 $FWD
@@ -33,7 +40,11 @@ if [ ! -f $REVNOCHIMERA ]; then
  if [ -f $REV.bz2 ]; then
   pbzip -d $REV.bz2
  elif [ ! -f $REV ]; then
-  zcat ITS_R1/ITS.*.fa.gz | fasta_formatter | fastx_trimmer -t 14 > $REV
+  if [ -f rev_adaptors.fa && -f fwd_adaptors.fa ]; then
+   zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -a file:rev_adaptors.fa -g file:fwd_adaptors.fa -o $REV -r $PREFIX.R2.rest.fna - > cutadapt_R2.log
+  else
+   zcat ITS_R1/ITS.*.fa.gz | fasta_formatter | fastx_trimmer -t 14 > $REV
+  fi
  fi
  vsearch -uchime_ref $REV -db $UCHIMECHIMERA -strand plus -nonchimeras $REVNOCHIMERA -threads $CPU
  pbzip2 $REV
