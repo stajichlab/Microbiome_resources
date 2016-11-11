d20c77e6bd220fb2716fb729c2e532c511c5d621
Jason Stajich <jason@bioperl.org>
2016-11-10 23:14:16 -0800

    work from fastq and work via kennedy lab protocol

--- scripts/00_make_combined_ITS.sh
+++ scripts/00_make_combined_ITS.sh
@@ -1,8 +1,9 @@
 #PBS -l nodes=1:ppn=16,mem=8gb -j oe
 module load fastx_toolkit
 module load vsearch
+module load trimmomatic
 # cutadapt is part of installed python
-
+MINLEN=125
 # merge reads previously split by sample into super file, assume reads can be matched to sample by their ID, eg PREFIX_readnumber - our convention is PROJECT.SAMPLEID
 CPU=$PBS_NP
 if [ ! $CPU ]; then
@@ -10,28 +11,30 @@ if [ ! $CPU ]; then
 fi
 PWD=`pwd`
 PREFIX=`basename $PWD`"_ITS"
-FWD=$PREFIX.R1.fna
-REV=$PREFIX.R2.fna
+FWD=$PREFIX.R1.fq
+FWDTRIM=$PREFIX.R1_trimmed.fq
+FWDFA=$PREFIX.R1.fna
+REV=$PREFIX.R2.fq
+REVTRIM=$PREFIX.R2_trimmed.fq
+REVFA=$PREFIX.R2.fna
+
 FWDNOCHIMERA=$PREFIX.R1.nochimeras.fna
 REVNOCHIMERA=$PREFIX.R2.nochimeras.fna
 UCHIMECHIMERA=/srv/projects/db/UNITE/v7.1/uchime_reference_dataset/ITS1_ITS2_datasets/uchime_sh_refs_dynamic_develop_985_01.01.2016.ITS1.fasta
 
 # per https://docs.google.com/document/d/1b9sUBOhOzlJxvt6Uim3hQXwOghIhEKulREzoSv2nXlE/edit
 # trim first 30 bp of read
-# whenusing Kabir's primers
+# when using Kabir's primers
 if [ ! -f $FWDNOCHIMERA ]; then
  if [ -f $REV.bz2 ]; then
    pbzip -d $REV.bz2
  elif [ ! -f $FWD ]; then
-  if [ -f rev_adaptors.fa && -f fwd_adaptors.fa ]; then
-    zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -a file:rev_adaptors.fa -g file:fwd_adaptors.fa -o $FWD -r $PREFIX.F1.rest.fna - > cutadapt_R1.log
-  else
-  zcat ITS_R1/ITS.*.fa.gz | fasta_formatter | fastx_trimmer -f 30 | fastx_trimmer -t 29 > $FWD
-#    zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -o $FWD -r $PREFIX.F1.rest.fna - > cutadapt_R1.log
-  fi
+   zcat ITS_R1/ITS.*.fq.gz | cutadapt -q 20 -a GATCTCTTGGNTCTNGCATCGATGAAGAACG -e 0.2 -o $FWD --minimum-length $MINLEN - > cutadapt_R1.log
+   java -jar $TRIMMOMATIC SE -phred33 $FWD $FWDTRIM LEADING:20 TRAILING:20 MINLEN:125
+   fastq_to_fasta -Q33 < $FWDTRIM | fastx_trimmer -m $MINLEN > $FWDFA
  fi
- vsearch -uchime_ref $FWD -db $UCHIMECHIMERA -strand plus -nonchimeras $FWDNOCHIMERA -threads $CPU
- pbzip2 $FWD
+ vsearch -uchime_ref $FWDFA -db $UCHIMECHIMERA -strand plus -nonchimeras $FWDNOCHIMERA -threads $CPU
+ pbzip2 $FWDFA
 fi
 
 # per https://docs.google.com/document/d/1b9sUBOhOzlJxvt6Uim3hQXwOghIhEKulREzoSv2nXlE/edit
@@ -40,12 +43,11 @@ if [ ! -f $REVNOCHIMERA ]; then
  if [ -f $REV.bz2 ]; then
   pbzip -d $REV.bz2
  elif [ ! -f $REV ]; then
-  if [ -f rev_adaptors.fa && -f fwd_adaptors.fa ]; then
-   zcat ITS_R1/ITS.*.fa.gz | cutadapt -u 30 -f fasta -a file:rev_adaptors.fa -g file:fwd_adaptors.fa -o $REV -r $PREFIX.R2.rest.fna - > cutadapt_R2.log
-  else
-   zcat ITS_R1/ITS.*.fa.gz | fasta_formatter | fastx_trimmer -t 14 > $REV
-  fi
+   zcat ITS_R2/ITS.*.fq.gz | cutadapt -a GGAAACCTTGTTACGACTTTTACTTCCTCTAAATGACCAA -e 0.3 -q 20 -o $REV - > cutadapt_R2.log
+   java -jar $TRIMMOMATIC SE -phred33 $REV $REVTRIM LEADING:20 TRAILING:20 MINLEN:125
+   # in khmer / python
+   fastq_to_fasta -Q33 < $REVTRIM | fastx_trimmer -m $MINLEN > $REVFA
  fi
- vsearch -uchime_ref $REV -db $UCHIMECHIMERA -strand plus -nonchimeras $REVNOCHIMERA -threads $CPU
+ vsearch -uchime_ref $REVFA -db $UCHIMECHIMERA -strand plus -nonchimeras $REVNOCHIMERA -threads $CPU
  pbzip2 $REV
 fi
--- scripts/qiime_ITS.sh
+++ scripts/qiime_ITS.sh
@@ -1,12 +1,15 @@
 #PBS -l nodes=1:ppn=32,mem=64gb -j oe -N qiime.ITS
 CPU=$PBS_NP
+if [ ! $CPU ]; then
+ CPU=2
+fi
 module unload python
 module load qiime
 module load ncbi-blast/2.2.26
 source activate qiime
 
 # setup the input data - this concats the compressed files into a single file for fwd or rev reads
-bash scripts/00_make_combined_ITS.sh
+#bash scripts/00_make_combined_ITS.sh
 PWD=`pwd`
 PREFIX=`basename $PWD`"_ITS"
 FWD=$PREFIX.R1.fna
@@ -22,21 +25,16 @@ REVNOCHIMERA=$PREFIX.R2.nochimeras.fna
 UNITEDB=/srv/projects/db/UNITE/v7.1/sh_refs_qiime_ver7_dynamic_s_22.08.2016.fasta
 UNITETAX=/srv/projects/db/UNITE/v7.1/sh_taxonomy_qiime_ver7_dynamic_s_22.08.2016.txt
 
-UCHIMECHIMERA=/srv/projects/db/UNITE/v7.1/uchime_reference_dataset/ITS1_ITS2_datasets/uchime_sh_refs_dynamic_develop_985_01.01.2016.ITS1.fasta
-
-if [ ! -f $FWDNOCHIMERA ]; then
- vsearch -uchime_ref $FWD -db $UCHIMECHIMERA -strand plus -nonchimeras $FWDNOCHIMERA -threads $CPU
-fi
-
-if [ ! -f $REVNOCHIMERA ]; then
- vsearch -uchime_ref $REV -db $UCHIMECHIMERA -strand plus -nonchimeras $REVNOCHIMERA -threads $CPU
-fi
-
 # currently this pipeline is only going to process FWD reads
 
-OUT=qiime_ITS_openref_nochimera
+OUT=qiime_ITS_otus_uclust_nochimera
 TAXOUT=qiime_ITS_taxa_summary_nochimera
 
+if [ ! -d $OUT ]; then 
+ parallel_pick_otus_uclust_ref.py -i $FWDNOCHIMERA -o $OUT -r $UNITEDB -s 0.6 -O $CPU
+fi
+
+OUT=qiime_ITS_openref_nochimera
 #Pick OTUs against open ref ITS, use mutitthreaded where possible
 if [ ! -d $OUT ]; then
     if [ $CPU ]; then
